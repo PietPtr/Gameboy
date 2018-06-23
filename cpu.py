056158339db470aa8814f60a76f52e5d001c9b12
Pieter Staal <pieterstaal@outlook.com>
2018-06-23 22:08:37 +0200

    Add write registers functions

--- cpu.py
+++ cpu.py
@@ -1,6 +1,15 @@
 import memory as m
 
+A = 0
+F = 1
+B = 2
+C = 3
+D = 4
+E = 5
+H = 6
+L = 7
 
+regs = [0, 0, 0, 0, 0, 0, 0, 0]
 
 ops = \
 {
@@ -9,9 +18,29 @@ ops = \
 
 def run():
     pc = 256
+    sp = 0
 
     while True:
         opcode = hex(m.read(pc))
         print(opcode)
 
         pc += 1
+
+def printRegs():
+    names = "AFBCDEHL"
+    for i in range(0, L+1):
+        print(str(names[i]) + ": ", regs[i], hex(regs[i]))
+
+def writeReg(reg, value):
+    assert reg >= 2 and value < 2**8
+
+    regs[reg] = value
+
+def writeRegs(reg, value):
+    assert reg % 2 == 0 and reg != 0 and value < 2**16
+
+    msbits = value >> 8
+    lsbits = value & 255
+
+    regs[reg] = msbits
+    regs[reg+1] = lsbits
